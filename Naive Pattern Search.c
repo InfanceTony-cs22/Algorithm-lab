d8460e897eae0adc5b502cc18e83074d0f47003f
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2024-08-04 13:42:55 +0530

    Create Naive Pattern Search.c

---
+++ Naive Pattern Search.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+void naiveSearch(char* txt, char* pat) {
+    int M = strlen(pat);
+    int N = strlen(txt);
+    for (int i = 0; i <= N - M; i++) {
+        int j;
+        for (j = 0; j < M; j++) {
+            if (txt[i + j] != pat[j])
+                break;
+        }
+        if (j == M) 
+            printf("Pattern found at index %d\n", i);
+    }
+}
+
+int main() {
+    char txt[] = "AABAACAADAABAAABAA";
+    char pat[] = "AABA";
+    naiveSearch(txt, pat);
+    return 0;
+}
